1f3ebc65c3a486746109fb8747cd25c44ce1b777
root <root@heera.c.mystical-nimbus-169411.internal>
2017-06-06 07:00:10 +0000

    commit

---
+++ assignment5.py
@@ -0,0 +1,10 @@
+#5. Define a function that can accept two strings as input and concatenate them and then print it in console.
+string1=input("enter string1")
+string2=input("enter string2")
+string=(string1+string2)
+print(string)
+
+'''OUTPUT:
+enter string1"heera"
+enter string2"patil"
+heerapatil'''
