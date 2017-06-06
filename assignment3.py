702dcc2f3fba693a8baa63ffa9f7baf899594a36
heerapatil <heera.patil1306@gmail.com>
2017-06-06 17:05:21 +0530

    Create assignment3.py

---
+++ assignment3.py
@@ -0,0 +1,33 @@
+import sqlite3
+conn=sqlite3.connect("first.db")
+cursor=conn.cursor()
+sql = CREATE TABLE EMPLOYEE( FIRST_NAME  CHAR(20) NOT NULL,LAST_NAME  CHAR(20), AGE INT,INCOME FLOAT )
+cursor.execute(sql)
+print "table created successfully"
+cursor.executemany("insert into employee(first_name,last_name,age,income)/values(%s,%s,%s,%s),
+[
+("heera","patil",21,25000),
+("ajay","musham",22,25000),
+print "insertion done"
+cursor.execute("select * from employee")
+for x in cursor:
+  print "FIRST_NAME = ", x[0] 
+  print "LAST_NAME = ", x[1] 
+  print "AGE = ", x[2]
+  print "INCOME= ", x[3],"\n" 
+conn.commit()
+print "record updated"
+cursor.execute("insert into employee(first_name,last_name,age,income)/ values("prachi","chikshe",24,25000)")
+print "insertion done"
+cursor.execute("delete from employee where age=24")
+conn.commit()
+print "record deleted"
+cursor.execute("select * from employee")
+for x in cursor:
+  print "FIRST_NAME = ", x[0] 
+  print "LAST_NAME = ", x[1] 
+  print "AGE = ", x[2]
+  print "INCOME= ", x[3],"\n" 
+conn.commit()
+conn.close()
+
