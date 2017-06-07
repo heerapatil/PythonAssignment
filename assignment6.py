54c1954af5053b74a6d6a64cc7b604019a0b65db
root <root@heera.c.mystical-nimbus-169411.internal>
2017-06-07 07:02:31 +0000

    commit

---
+++ assignment6.py
@@ -0,0 +1,20 @@
+#Write a program that takes inputs from user their name and their age. And display the year in which they will turn 100 years old.
+import time
+import datetime
+Name=raw_input("Enter your Name:")
+Age=int(input("Enter your Age:"))
+print 'Hi %s ,Your present Age is %d'%(Name,Age)  
+#cd=time.strftime("%d/%m/%Y") 
+#cd-age 
+#print cd.year
+cd1=datetime.datetime.now()
+cdy=cd1.year
+by=int(cdy-Age)
+print 'Your Bday year is %d'%by
+by1=int(by+100)
+print 'Your age will be 100 years by %d'%by1
+
+'''OUTPUT:
+
+Enter your Name:"heera"Enter your Age:21Hi "heera" ,Your present Age is 21Your Bday year is 1996Your age will be 100 years by 2096
+'''
