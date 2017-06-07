c6838cba2d2c0e7dd02e86a78086275b7dc30d82
root <root@heera.c.mystical-nimbus-169411.internal>
2017-06-07 07:12:56 +0000

    commit

---
+++ assignment20.py
@@ -0,0 +1,10 @@
+#20. Ask the user for a string and print out whether this string is a palindrome or not
+string="madam"
+if string==string[::-1]:
+ print"It is a palindrome"
+else:
+ print "It is not a palindrome"
+
+'''output:
+It is a palindrome
+'''
